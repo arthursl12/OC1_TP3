c710098d50aaaeceddbb092495f6822da6876da8
arthursl12 <arthursoutolima@gmail.com>
2020-10-29 10:14:52 -0300

    Parse inicial dos argumentos

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import argparse
+
 """
 Entrada: um arquivo .txt, obtido dos argumentos, com as instruções
 Saída: outro arquivo .txt, com os resultados das instruções
 """
 def main():
-    pass
+    # Lê os argumentos para obter nome dos arquivos de entrada e saída
+    parser = argparse.ArgumentParser()
+    parser.add_argument("arq_entrada", help="Nome arquivo entrada")
+    parser.add_argument(
+        "-s",
+        "--arquivo-saida",
+        action="store",
+        type=str,
+        dest="arq_saida",
+        help="(Opcional) Nome arquivo entrada",
+        default="out.txt"
+    )
+    args = parser.parse_args()
+    print(args.arq_entrada)
+    print(args.arq_saida)
 
 if __name__ == "__main__":
     main()
